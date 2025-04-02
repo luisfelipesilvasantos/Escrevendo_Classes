a6b00a5b51b94d6015b746ee3cf474db1640d90d
luisfelipesilvasantos <luisflp.375@gmail.com>
2025-04-02 00:37:23 -0300

    Segundo Commit

--- index.js
+++ index.js
@@ -14,12 +14,18 @@ class Heroi {
         };
 
         let ataque = ataques[this.tipo] || "um ataque desconhecido";
-        console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}.`);
+        console.log(`O ${this.tipo} ${this.nome} de ${this.idade} anos atacou usando ${ataque}.`);
     }
 }
 
 // Criando uma instância da classe
 let ataque = new Heroi("Aragorn", 30, "Guerreiro");
+let ataque1 = new Heroi("Gandalf", 20, "Mago");
+let ataque2 = new Heroi("Tenzin", 33, "Monge");
+let ataque3 = new Heroi("Hanzo", 23, "Ninja");
 
 // Chamando o método atacar
 ataque.atacar();
+ataque1.atacar();
+ataque2.atacar();
+ataque3.atacar();
